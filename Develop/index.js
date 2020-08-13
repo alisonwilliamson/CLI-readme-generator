8cdeec87601284ac11bccf3af0f901d4ce5936c8
Allie <williamson.alisonj@gmail.com>
2020-08-13 19:49:08 -0400

    created markdown file

--- Develop/index.js
+++ Develop/index.js
@@ -1,66 +1,84 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
-// array of questions for users
+// array of questions for user
 const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?",
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub username?",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?",
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Please write a short description of your project",
-    },
-    {
-        type: "input",
-        name: "license",
-        message: "What kind of license do you need? You can choose ISC, MIT, Apache License 2.0, GNU GPLv2, GNU GPLv3.",
-    },
-    {
-        type: "input",
-        name: "dependencies",
-        message: "What command should be used to run dependencies?",
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "What command should be used to run tests?",
-    },
-    {
-        type: "input",
-        name: "repo",
-        message: "What does the user need to know about using the repo?",
-    },
-    {
-        type: "input",
-        name: "contributions",
-        message: "Provide list of contributors.",
-    },
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Write a short description of your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What is the installation process?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "How will this project be used?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Provide a list of contributors.",
+    name: "contributors",
+  },
+  {
+    type: "input",
+    message: "What command should be used to run dependencies?",
+    name: "dependencies",
+  },
+  {
+    type: "input",
+    message: "What kind of license do you need? You can choose ISC, MIT, Apache License 2.0, GNU GPLv2, GNU GPLv3.",
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "What is your Github username?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your Github profile url?",
+    name: "url",
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "How should you run tests?",
+    name: "tests",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+async function writeToFile(filename, data) {
+  fs.writeFile(filename,data, function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  })
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((results) => {
+    const readTemplate = generateMarkdown(results);
+    writeToFile("README.md", readTemplate);
+    return "success"
+  });
 }
 
 // function call to initialize program
 init();
-
-
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,26 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+   return `# Title: ${data.title}
+##Table of Contents
+* [Installation](#installation)               
+* [Usage](#usage)                    
+* [License](#license)                      
+* [Contributor](#contributors)         
+* [Question](#question) 
+## Description:
+${data.description}
+## Installation:
+${data.installation}
+## Usage:
+${data.usage}
+## Contributions:
+${data.contributors}
+## Testing:
+${data.tests}
+## License:
+${data.license}
+## Questions:
+${data.github} , ${data.email}
 `;
 }
 
